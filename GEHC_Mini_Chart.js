28d68deed729221bf7356f2ce64be653a23a1d43
mandisarala <129859356+mandisarala@users.noreply.github.com>
2023-04-25 16:37:43 +0530

    Mini chart

---
+++ GEHC_Mini_Chart.js
@@ -0,0 +1,20 @@
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-GEHC/" rel="noopener" target="_blank"><span class="blue-text">GEHC stock price</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
+  {
+  "symbol": "NASDAQ:GEHC",
+  "width": "350",
+  "height": "220",
+  "locale": "en",
+  "dateRange": "12M",
+  "colorTheme": "light",
+  "isTransparent": true,
+  "autosize": false,
+  "largeChartUrl": "",
+  "noTimeScale": false
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
